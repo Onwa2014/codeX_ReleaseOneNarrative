5305b6777773de5d4e7d0bfadc49b2c0730fb0b5
Onwaba <onwaba@projectcodex.co>
2015-04-02 17:02:20 +0200

    Modified

--- test/products_test.js
+++ test/products_test.js
@@ -109,4 +109,50 @@ describe("Find the most popular product", function(){
 
             assert.deepEqual(result, productsResults);      
     });
+
+    it('should return the most popular category', function(){
+
+        var products = new Products();
+
+            var productMap = {
+              'Milk 1l': 142,
+              'Imasi': 125,
+              'Bread': 130,
+              'Chakalaka Can': 94,
+              'Gold Dish Vegetable Curry Can': 86,
+              'Fanta 500ml': 94,
+              'Coke 500ml': 159,
+              'Cream Soda 500ml': 75,
+              'Iwisa Pap 5kg': 47,
+              'Top Class Soy Mince': 98,
+              'Shampoo 1 litre': 26,
+              'Soap Bar': 50,
+              'Bananas - loose': 114,
+              'Apples - loose': 114,
+              'Mixed Sweets 5s': 172,
+              'Heart Chocolates': 20,
+              'Rose (plastic)': 14,
+              'Valentine Cards': 14 }
+
+        
+           
+        var catMap = {
+          'Dairy Products': 267,
+          'Bakery': 130,
+          'Bulk': 47,
+          'Canfectionery': 192,
+          'Cosmetics': 76,
+          'Fruits': 128,
+          'Cold Beverages': 328,
+          'Canned Food': 180,
+          'Valentines Goodies': 28,
+          'Soup': 98 }
+
+           var result = {name: 'Cold Beverages', amt:328}; //
+           var mostPopularCategory = products.mostPopularCat(productMap);
+           
+           //console.log(mostPopularCategory);
+
+            assert.deepEqual(result, mostPopularCategory);      
+    });
 });
